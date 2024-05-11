9004d99f91531619b2a3477a2c9dad746553039f
Tal Alon <talalon369@gmail.com>
2024-05-12 00:48:43 +0300

    added ecr resources

---
+++ terraform/aws_ecr.tf
@@ -0,0 +1,26 @@
+resource "aws_ecr_repository" "ac-users-api" {
+  name = "ac-users-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "ac-courses-api" {
+  name = "ac-courses-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "ac-files-api" {
+  name = "ac-files-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
+variable "aws_session_token" {
+  description = "AWS session token"
+}
